fe4f32249b31770b835d845eb4ff00923193fe88
testinnplayin <archaurore@gmail.com>
2020-11-23 17:19:28 +0100

    feat(basic): Added test to ensure new file per logger trigger.

--- __tests__/test-helpers.ts
+++ __tests__/test-helpers.ts
@@ -1,5 +1,17 @@
-import { readFile } from "fs";
+/**
+ * Contains helpers for tests
+ *
+ * Author: R.Wood
+ * 23/11/2020
+ */
 
+import { readdir, readFile } from "fs";
+
+/**
+ * reads file at given test filepath
+ * @param {string} testFilePath - the test file's filepath
+ * @returns a promise
+ */
 function doReadFile(testFilePath: string): Promise<any> {
   return new Promise((resolve, reject) => {
     readFile(testFilePath, { encoding: "utf8" }, (err: any, data: string) => {
@@ -9,4 +21,18 @@ function doReadFile(testFilePath: string): Promise<any> {
   });
 }
 
-export { doReadFile };
+/**
+ * reads contents of given directory
+ * @param {string} dirPath - the test directory path
+ * @returns a promise
+ */
+function doReadDir(dirPath: string): Promise<any> {
+  return new Promise((resolve, reject) => {
+    readdir(dirPath, (err: any, files: any) => {
+      if (err) reject(err);
+      return resolve(files);
+    });
+  });
+}
+
+export { doReadFile, doReadDir };
