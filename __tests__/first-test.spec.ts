8bf84453329d0b763d53eda0f8d41442e09c0932
testinnplayin <archaurore@gmail.com>
2020-11-25 17:38:55 +0100

    feat(basic): Add message template.

--- __tests__/first-test.spec.ts
+++
@@ -1,5 +0,0 @@
-describe("#First test", () => {
-  it("should pass if set up correctly", () => {
-    expect(1).toBe(1);
-  });
-});
--- src/simple-logger.ts
+++ src/simple-logger.ts
@@ -1,14 +1,26 @@
 import { writeFile } from "fs";
 
+enum LogLevel {
+  INFO = "INFO",
+}
+
+/**
+ * @interface MessageTemplate - template for a SimpleLogger message
+ */
 interface MessageTemplate {
+  /** timestamp in UTC ISODate format */
+  timestamp?: string;
+  /** log level, by default 'INFO' */
+  level?: LogLevel;
+  /** actual text message */
   message: string;
 }
 
 export default {
   /**
    * Build a file name from a date time string and a temporary file name
-   * @param {string} dateTime - a datetime string in YYYY-MM-DD format
-   * @param {string | null} fileName - the temporary file name
+   * @param dateTime - a datetime string in YYYY-MM-DD format
+   * @param fileName - the temporary file name with 'my_file' as default
    * @returns a string in the format of 'my_file-YYYY-MM-DD'
    */
   buildFileName(dateTime: string, fileName: string | null): string {
@@ -21,18 +33,27 @@ export default {
 
   /**
    * Build a file path from the log directory path and the file name
-   * @param {string} logsDir - the absolute path to the logs directory
-   * @param {string} fileName - the name of the file
+   * @param logsDir - the absolute path to the logs directory
+   * @param fileName - the name of the file
    * @returns an absolute path for the file path of the log file
    */
   buildFilePath(logsDir: string, fileName: string): string {
     return `${logsDir}/${fileName}`;
   },
 
+  buildMessage(messageTemplate: MessageTemplate): MessageTemplate {
+    const timestamp: Date = new Date(Date.now());
+
+    messageTemplate.timestamp = timestamp.toISOString();
+    messageTemplate.level = LogLevel.INFO;
+
+    return messageTemplate;
+  },
+
   /**
    * Main function for writing a message to a file
-   * @param {string} filePath - absolute path to where file will be written
-   * @param {string} messageTemplate - message written in file
+   * @param filePath - absolute path to where file will be written
+   * @param messageTemplate - message written in file
    * @returns a Promise containing the message
    */
   triggerLogger(
@@ -40,6 +61,7 @@ export default {
     messageTemplate: MessageTemplate
   ): Promise<string> {
     console.log(messageTemplate.message);
+    messageTemplate = this.buildMessage(messageTemplate);
 
     return new Promise((resolve, reject) => {
       writeFile(
